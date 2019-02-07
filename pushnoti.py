814cb3b7b75eac65d3449bec270ff71a71412090
Bhaumik Ichhaporia <bhaumik.ichhaporia59@gmail.com>
2019-02-06 16:52:21 -0800

    Update pushnoti.py

--- pushnoti.py
+++ pushnoti.py
@@ -8,8 +8,8 @@ push_service = FCMNotification(api_key="AAAAHrwNmUA:APA91bEBJ78FXFZTqZCJHRmXChjQ
 
 #registration_id = "clbXlKCbNCY:APA91bHwqA7ev9Q_-uMfPFhkvS1V6Rn3pNaxDl-C6T-4uta0sxb9-GyPcQn7eJrzkB8MoNw4GRTbM7cmLKb5F98Un3lSl15Y3mwY6OIBdthucwvyfffHxJwVoxgJtOZk08Otj-bXTN0Z"
 registration_id ="/topics/machine1"
-message_title = "Uber update"
-message_body = "Hi john, your customized news for today is ready"
+message_title = "Intrusion update"
+message_body = "Intrusion Detected"
 result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
 
 print( result)
